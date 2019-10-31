856efd7b4b736241d687f11df86dad4eb0914b2f
nataliawcislo <49069886+nataliawcislo@users.noreply.github.com>
2019-10-31 21:57:41 +0100

    Update CastleTableViewController.swift

--- TheCastle/TheCastle/CastleTableViewController.swift
+++ TheCastle/TheCastle/CastleTableViewController.swift
@@ -18,25 +18,21 @@ class CastleTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Generate the animal dictionary
+
         createCastleDict()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     // MARK: - Table view data source
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // Return the number of sections.
         return castleSectionTitles.count
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // Return the number of rows in the section.
         let castleKey = castleSectionTitles[section]
         guard let castleValues = castlesDict[castleKey] else {
             return 0
@@ -60,14 +56,10 @@ class CastleTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
-        // Configure the cell...
+
         let castleKey = castleSectionTitles[indexPath.section]
         if let castleValues = castlesDict[castleKey] {
             cell.textLabel?.text = castleValues[indexPath.row]
-            
-            // Convert the animal name to lower case and
-            // then replace all occurrences of a space with an underscore
             let imageFilename = castleValues[indexPath.row].lowercased().replacingOccurrences(of: " ", with: "_")
             cell.imageView?.image = UIImage(named: imageFilename)
         }
@@ -97,7 +89,6 @@ class CastleTableViewController: UITableViewController {
     
     func createCastleDict() {
         for castle in castles {
-            // Get the first letter of the animal name and build the dictionary
             let firstLetterIndex = castle.index(castle.startIndex, offsetBy: 1)
             let castleKey = String(castle[..<firstLetterIndex])
             
@@ -108,8 +99,6 @@ class CastleTableViewController: UITableViewController {
                 castlesDict[castleKey] = [castle]
             }
         }
-        
-        // Get the section titles from the dictionary's keys and sort them in ascending order
         castleSectionTitles = [String](castlesDict.keys)
         castleSectionTitles = castleSectionTitles.sorted(by: { $0 < $1 })
     }
